9cdbfb8b73468f826a12a0405d395fe1885fc85e
Ankit kumar <ankit.kumar.info22@gmail.com>
2024-07-07 14:22:30 +0530

    added stripe payment method

--- medusa-config.js
+++ medusa-config.js
@@ -52,6 +52,14 @@ const plugins = [
       },
     },
   },
+  {
+    resolve: `medusa-payment-stripe`,
+    options: {
+      api_key: process.env.STRIPE_API_KEY,
+      payment_description: "Simkit Payment",
+      // webhook_secret: process.env.STRIPE_WEBHOOK_SECRET,
+    },
+  },
 ];
 
 const modules = {
